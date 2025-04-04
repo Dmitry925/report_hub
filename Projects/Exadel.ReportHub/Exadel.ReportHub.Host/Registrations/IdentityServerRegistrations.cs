71c6ff65f3fa0ed8d3d00ca7985821d97026aa46
Dmitry917 <rogowdmitriy917@gmail.com>
2025-04-04 22:56:43 +0300

    update

--- Projects/Exadel.ReportHub/Exadel.ReportHub.Host/Registrations/IdentityServerRegistrations.cs
+++ Projects/Exadel.ReportHub/Exadel.ReportHub.Host/Registrations/IdentityServerRegistrations.cs
@@ -12,6 +12,7 @@ public static class IdentityServerRegistrations
     {
         services.AddIdentityServer(options =>
         {
+            options.IssuerUri = configuration["Authority"];
         })
             .AddClientStore<IdentityClientStore>()
             .AddResourceStore<IdentityResourceStore>()
